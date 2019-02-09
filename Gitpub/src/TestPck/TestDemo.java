60a09ffdcd3548d79cb6ae2001be033c592ec156
alokindia <47476019+alokindia@users.noreply.github.com>
2019-02-09 18:01:49 +0530

    Update TestDemo.java

--- Gitpub/src/TestPck/TestDemo.java
+++ Gitpub/src/TestPck/TestDemo.java
@@ -5,6 +5,8 @@ public class TestDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Alok");
+			System.out.println("Hello Alowewk");
+
 
 	}
 
